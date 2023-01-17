64450218d50e19e5a1c8920d9ef026a5743291d9
WaiLinnAung18 <waia00198@gmail.com>
2023-01-17 11:35:58 +0630

    update2

--- src/Nav/Navbar.jsx
+++ src/Nav/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {RiRestaurant2Fill} from 'react-icons/ri'
 const Navbar = () => {
   return (
-    <div className=' flex items-center my-5 ml-36 md:ml-0'>
+    <div className=' flex items-center my-5 ml-28 md:ml-0'>
         <RiRestaurant2Fill className=' text-4xl text-orange-500'/>
         <p className='text-3xl text-orange-500 font-bold'>How2Cook</p>
     </div>
